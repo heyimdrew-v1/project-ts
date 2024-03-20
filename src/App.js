8ab388a114a691358a453dfb5628cceeb263a5e2
Drew Swanson <39685941+heyimdrew-v1@users.noreply.github.com>
2024-03-19 23:11:50 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Welcome to my React app",
+      counter: 0
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.message}</h1>
+        <button onClick={this.handleButtonClick}>Click me</button>
+        <p>You have clicked the button {this.state.counter} times.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
